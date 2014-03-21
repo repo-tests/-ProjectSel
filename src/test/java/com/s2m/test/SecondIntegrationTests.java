1b678fb44931bf25536975059339de85119afbb4
“repo-tests” <algebre07@hotmail.fr>
2014-03-21 14:29:41 +0000

    9Changement

--- src/test/java/com/s2m/test/SecondIntegrationTests.java
+++ src/test/java/com/s2m/test/SecondIntegrationTests.java
@@ -13,7 +13,7 @@ import com.thoughtworks.selenium.Selenium;
 
 		@Before
 		public void setUp() throws Exception {
-			selenium = new DefaultSelenium("localhost", 4444, "*chrome", "http://10.0.0.107:8080/selectsystem-view-tomcat-oracle/login.xhtml");
+			selenium = new DefaultSelenium("localhost", 4444, "*firefox", "http://10.0.0.107:8080/selectsystem-view-tomcat-oracle/login.xhtml");
 			selenium.start();
 		}
 
@@ -29,8 +29,8 @@ import com.thoughtworks.selenium.Selenium;
 			selenium.waitForPageToLoad("30000");
 			selenium.click("link=New fee");
 			selenium.waitForPageToLoad("30000");
-			selenium.type("id=globalcardHolderCommissionForm:iden", "7");
-			selenium.type("id=globalcardHolderCommissionForm:label", "nom7");
+			selenium.type("id=globalcardHolderCommissionForm:iden", "22");
+			selenium.type("id=globalcardHolderCommissionForm:label", "nom22");
 			selenium.type("id=globalcardHolderCommissionForm:rate", "11");
 			selenium.type("id=globalcardHolderCommissionForm:fixe", "25");
 			selenium.type("id=globalcardHolderCommissionForm:min", "100");
